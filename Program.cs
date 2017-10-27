cb122b23ad8294df02d2adf70c965d98a44dd219
Uwe Riegel <uwriegel@gmail.com>
2017-10-27 15:52:51 +0200

    program started

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
+namespace Commander.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Console.Error.WriteLine("Commander.net started");
+                var handle = GetStdHandle(-11);
+                var strom = new FileStream(handle, FileAccess.Write);
+                
+                while (true)
+                {
+                    var line = Console.ReadLine();
+                    switch (line) 
+                    {
+                        case "getDrives":
+                        
+                            break;
+                        case "exit":
+                            return;
+                    }
+                }
+            }
+            catch (Exception e) 
+            {
+                Console.Error.WriteLine(e);
+            }
+            finally
+            {
+                Console.Error.WriteLine("Commander.net stopped");
+            }
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern SafeFileHandle GetStdHandle(int nStdHandle);
+    }
+}
